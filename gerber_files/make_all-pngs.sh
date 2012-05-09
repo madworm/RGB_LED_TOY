8fcc9a6a9ded662072e3efb13345bb2455fee008
Robert Spitzenpfeil <robert@linuxbox.site>
2012-05-09 11:05:08 +0200

    Shifted a header a bit + shell-script mods.

--- gerber_files/make_all-pngs.sh
+++ gerber_files/make_all-pngs.sh
@@ -8,4 +8,12 @@ for file in `ls *.drl`; do
 gerbv $file -o $file.png --dpi=300x300 -a --export=png
 done
 
+for file in `ls ./gerbv/*.gvp`; do
+# replace matching from the beginning of $file
+OUTFILE=${file/#.\/*\//.\/PNGs/}
+# replace matching from the end of $OUTFILE
+OUTFILE=${OUTFILE/%.gvp/.png}
+gerbv -p $file -o $OUTFILE --dpi=300x300 -a --export=png
+done
+
 mv *.png ./PNGs
